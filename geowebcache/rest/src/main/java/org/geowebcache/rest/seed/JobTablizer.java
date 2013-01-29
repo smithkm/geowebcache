7cc354c53d5f0869d9131cc26f99955643caa1bf
Kevin Smith <ksmith@opengeo.org>
2013-01-29 11:11:10 -0800

    Extensible columns for tablizer

--- geowebcache/rest/src/main/java/org/geowebcache/rest/seed/JobTablizer.java
+++ geowebcache/rest/src/main/java/org/geowebcache/rest/seed/JobTablizer.java
@@ -2,9 +2,11 @@ package org.geowebcache.rest.seed;
 
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Locale;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.geowebcache.layer.TileLayer;
 import org.geowebcache.seed.GWCTask.STATE;
 import org.geowebcache.seed.JobStatus;
@@ -26,7 +28,89 @@ public class JobTablizer {
         this.doc = doc;
         this.tl = tl;
     }
+    
+    abstract class Column {
+        public Column(String header) {
+            this.header=header;
+        }
+        
+        final private String header;
+        public String getHeader(){
+            return header;
+        }
+        
+        public abstract String getField(JobStatus job, TaskStatus task);
+    }
 
+    protected final Column id = new Column("ID"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return Long.toString(task.getTaskId());
+        }
+    };
+    protected final Column layer = new Column("Layer"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return job.getLayerName();
+        }
+    };
+    protected final Column state = new Column("State"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return task.getState().toString();
+        }
+    };
+    protected final Column type  = new Column("type"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return job.getType().toString();
+        }
+    };
+    protected final Column total_tiles  = new Column("Estimated # of tiles"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return Long.toString(task.getTilesTotal());
+        }
+    };
+    protected final Column cmplt_tiles  = new Column("Tiles Completed"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return Long.toString(task.getTilesDone());
+        }
+    };
+    protected final Column elapsed_time  = new Column("Time Elapsed"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return SeedFormRestlet.toTimeString(task.getTimeSpent(), task.getTilesDone(), task.getTilesTotal());
+        }
+    };
+    protected final Column remaining_time  = new Column("Time Remaining"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return SeedFormRestlet.toTimeString(task.getTimeRemaining(), task.getTilesDone(), task.getTilesTotal());
+        }
+    };
+    protected final Column job_tasks  = new Column("Tasks"){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return Long.toString(job.getThreadCount());
+        }
+    };
+    protected final Column kill  = new Column(null){
+        @Override
+        public String getField(JobStatus job, TaskStatus task) {
+            return makeThreadKillForm(task.getTaskId(), tl);
+        }
+    };
+    
+    Column[] firstColumns = {id};
+    Column[] midColumns = {layer, state, type, total_tiles, cmplt_tiles, elapsed_time, remaining_time};
+    Column[] lastColumns = {kill};
+   
+    private int getColumnCount(){
+        return firstColumns.length+midColumns.length+lastColumns.length;
+    }
+    
     protected void head() throws IOException {
         doc.append("<thead>");
         doc.append("<tr>");
@@ -36,22 +120,41 @@ public class JobTablizer {
         doc.append("</tr>");
         doc.append("</thead>");
     }
-    //doc.append("<thead><tr><th style=\"padding-right:20px;\">Id</th><th style=\"padding-right:20px;\">Layer</th><th style=\"padding-right:20px;\">Status</th><th style=\"padding-right:20px;\">Type</th><th>Estimated # of tiles</th>"
-    //        + "<th style=\"padding-right:20px;\">Tiles completed</th><th style=\"padding-right:20px;\">Time elapsed</th><th>Time remaining</th><th>Tasks</th><th>&nbsp;</th>");
     
-    public static final String[] HEADERS = {"ID", "Layer", "Status", "Type", "Estimated # of tiles", "Tiles completed", "Time elapsed", "Time remaining", "Tasks", null};
     protected void columnHeaders() throws IOException {
         
-        for(String title: HEADERS){
-            columnHeader(title);
+        for(Column col: firstColumns){
+            columnHeader(col.getHeader());
         }
-    }
+        for(Column col: midColumns){
+            columnHeader(col.getHeader());
+        }
+        for(Column col: lastColumns){
+            columnHeader(col.getHeader());
+        }
+   }
     
     protected void columnHeader(String title) throws IOException {
         if(title!=null){
             doc.append("<th scope=\"col\">").append(title).append("</th>");
         } else {
-            doc.append("<th/>");
+            doc.append("<th scope=\"col\"/>");
+        }
+    }
+    protected void cell(String data, String scope) throws IOException {
+        if(data!=null){
+            if(scope!=null){
+                doc.append("<td scope=\"").append(scope).append("\">");
+            } else {
+                doc.append("<td>");
+            }
+            doc.append(data).append("</th>");
+        } else {
+            if(scope!=null){
+                doc.append("<td scope=\"").append(scope).append("\"/>");
+            } else {
+                doc.append("<td/>");
+            }
         }
     }
     
@@ -68,7 +171,7 @@ public class JobTablizer {
     
     protected void jobRow(JobStatus job) throws IOException {
         doc.append("<tr class=\"job\">");
-        doc.append("<th colspan=\"").append(Integer.toString(HEADERS.length-1)).append("\" scope=\"rowgroup\">Job ");
+        doc.append("<th colspan=\"").append(Integer.toString(getColumnCount()-1)).append("\" scope=\"rowgroup\">Job ");
         doc.append(Long.toString(job.getJobId()));
         doc.append("</th>");
         doc.append("<td><s>Kill Job</s></td>"); // TODO
@@ -77,49 +180,16 @@ public class JobTablizer {
 
     int taskRowCounter = 0;
     protected void taskRow(TaskStatus task, JobStatus job) throws IOException {
-        final long spent = task.getTimeSpent();
-        final long remining = task.getTimeRemaining();
-        final long tilesDone = task.getTilesDone();
-        final long tilesTotal = task.getTilesTotal();
-
-        NumberFormat nf = NumberFormat.getInstance(Locale.ENGLISH);
-        nf.setGroupingUsed(true);
-        final String tilesTotalStr;
-        if (tilesTotal < 0) {
-            tilesTotalStr = "Too many to count";
-        } else {
-            tilesTotalStr = nf.format(tilesTotal);
-        }
-        final String tilesDoneStr = nf.format(task.getTilesDone());
-        final STATE state = task.getState();
-
-        final String status = STATE.UNSET.equals(state) || STATE.READY.equals(state) ? "PENDING"
-                : state.toString();
-
-        String timeSpent = SeedFormRestlet.toTimeString(spent, tilesDone, tilesTotal);
-        String timeRemaining = SeedFormRestlet.toTimeString(remining, tilesDone, tilesTotal);
-
-        String layerName = tl.getName();
-        
         doc.append("<tr class=\"task ").append(taskRowCounter%2==0?"even":"odd").append("\">");
-        doc.append("<td scope=\"row\">").append(Long.toString(task.getTaskId())).append("</td>");
-        doc.append("<td>");
-        if (!layerName.equals(job.getLayerName())) {
-            doc.append("<a href=\"./").append(job.getLayerName()).append("\">");
+        for(Column col: firstColumns){
+            cell(col.getField(job, task), "row");
         }
-        doc.append(job.getLayerName());
-        if (!layerName.equals(job.getLayerName())) {
-            doc.append("</a>");
+        for(Column col: midColumns){
+            cell(col.getField(job, task), null);
         }
-        doc.append("</td>");
-        doc.append("<td>").append(status).append("</td>");
-        doc.append("<td>").append(job.getType().toString()).append("</td>");
-        doc.append("<td>").append(tilesTotalStr).append("</td>");
-        doc.append("<td>").append(tilesDoneStr).append("</td>");
-        doc.append("<td>").append(timeSpent).append("</td>");
-        doc.append("<td>").append(timeRemaining).append("</td>");
-        doc.append("<td>").append(Long.toString(job.getThreadCount())).append("</td>");
-        doc.append("<td>").append(makeThreadKillForm(task.getTaskId(), tl)).append("</td>");
+        for(Column col: lastColumns){
+            cell(col.getField(job, task), "row");
+        }
 
         doc.append("</tr>");
         
