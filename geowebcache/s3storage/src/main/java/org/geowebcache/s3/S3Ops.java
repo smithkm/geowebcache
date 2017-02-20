3492e1c6da60b25a074f5a20d5fee67d44cb1017
Kevin Smith <ksmith@boundlessgeo.com>
2017-02-20 14:34:27 -0800

    Storing parameters mappings in S3

--- geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3Ops.java
+++ geowebcache/s3storage/src/main/java/org/geowebcache/s3/S3Ops.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -31,9 +32,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.io.IOUtils;
 import org.geowebcache.GeoWebCacheException;
 import org.geowebcache.locks.LockProvider;
 import org.geowebcache.locks.LockProvider.Lock;
@@ -51,11 +54,11 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.google.common.base.Charsets;
-import com.google.common.base.Predicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
-import com.google.common.io.ByteStreams;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 class S3Ops {
@@ -269,7 +272,7 @@ class S3Ops {
             return null;
         }
         try (S3ObjectInputStream in = object.getObjectContent()) {
-            byte[] bytes = ByteStreams.toByteArray(in);
+            byte[] bytes = IOUtils.toByteArray(in);
             return bytes;
         } catch (IOException e) {
             throw new StorageException("Error getting " + key, e);
@@ -297,7 +300,7 @@ class S3Ops {
         if (bytes != null) {
             try {
                 properties.load(new InputStreamReader(new ByteArrayInputStream(bytes),
-                        Charsets.UTF_8));
+                        StandardCharsets.UTF_8));
             } catch (IOException e) {
                 throw Throwables.propagate(e);
             }
@@ -351,9 +354,11 @@ class S3Ops {
                         prefix, timestamp));
                 Iterable<S3ObjectSummary> objects = S3Objects.withPrefix(conn, bucketName, prefix);
                 Predicate<S3ObjectSummary> filter = new TimeStampFilter(timestamp);
-                objects = Iterables.filter(objects, filter);
-
-                Iterable<List<S3ObjectSummary>> partitions = Iterables.partition(objects, 1000);
+                AtomicInteger n = new AtomicInteger(0); 
+                Iterable<List<S3ObjectSummary>> partitions = StreamSupport.stream(objects.spliterator(), true)
+                        .filter(filter)
+                        .collect(Collectors.groupingBy((x)->n.getAndIncrement()%1000))
+                        .values();
 
                 for (List<S3ObjectSummary> partition : partitions) {
 
@@ -418,7 +423,7 @@ class S3Ops {
         }
 
         @Override
-        public boolean apply(S3ObjectSummary summary) {
+        public boolean test(S3ObjectSummary summary) {
             long lastModified = summary.getLastModified().getTime();
             boolean applies = timeStamp >= lastModified;
             return applies;
