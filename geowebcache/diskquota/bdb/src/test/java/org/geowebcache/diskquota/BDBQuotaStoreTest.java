454bf95c28b68bba10d280f91eac4f37aa677108
Kevin Smith <ksmith@boundlessgeo.com>
2018-02-14 18:16:24 -0800

    Fixing unit test for BDB Quota Store

--- geowebcache/diskquota/bdb/src/test/java/org/geowebcache/diskquota/BDBQuotaStoreTest.java
+++ geowebcache/diskquota/bdb/src/test/java/org/geowebcache/diskquota/BDBQuotaStoreTest.java
@@ -27,6 +27,7 @@ import java.util.stream.Stream;
 
 import org.easymock.Capture;
 import org.easymock.EasyMock;
+import org.geowebcache.MockWepAppContextRule;
 import org.geowebcache.config.BaseConfiguration;
 import org.geowebcache.config.MockConfigurationResourceProvider;
 import org.geowebcache.config.TileLayerConfiguration;
@@ -77,6 +78,8 @@ public class BDBQuotaStoreTest {
     
     @Rule
     public ExpectedException exception = ExpectedException.none();
+    @Rule
+    public MockWepAppContextRule context = new MockWepAppContextRule();
 
     Map<String, Set<String>> parameterIdsMap;
     Map<String, Set<Map<String, String>>> parametersMap;
@@ -113,13 +116,22 @@ public class BDBQuotaStoreTest {
                             .collect(Collectors.toSet())
                         ));
         XMLConfiguration xmlConfig = loadXMLConfig();
+        context.addBean("xmlConfig", xmlConfig, XMLConfiguration.class.getInterfaces());
         LinkedList<TileLayerConfiguration> configList = new LinkedList<TileLayerConfiguration>();
         configList.add(xmlConfig);
 
-        layerDispatcher = new TileLayerDispatcher(new GridSetBroker(true, true), configList);
+        GridSetBroker gridSetBroker = new GridSetBroker(true, true);
+        layerDispatcher = new TileLayerDispatcher(gridSetBroker);
+        layerDispatcher.setApplicationContext(context.getMockContext());
 
         tilePageCalculator = new TilePageCalculator(layerDispatcher, storageBroker);
-
+        
+        xmlConfig.setGridSetBroker(gridSetBroker);
+        
+        xmlConfig.afterPropertiesSet();
+        layerDispatcher.afterPropertiesSet();
+        gridSetBroker.afterPropertiesSet();
+        
         store = new BDBQuotaStore(cacheDirFinder, tilePageCalculator);
         store.startUp();
         testTileSet = tilePageCalculator.getTileSetsFor("topp:states2").iterator().next();
@@ -128,7 +140,7 @@ public class BDBQuotaStoreTest {
     private XMLConfiguration loadXMLConfig() {
         XMLConfiguration xmlConfig = null;
         try {
-            xmlConfig = new XMLConfiguration(null, new MockConfigurationResourceProvider(()->XMLConfiguration.class
+            xmlConfig = new XMLConfiguration(context.getContextProvider(), new MockConfigurationResourceProvider(()->XMLConfiguration.class
                 .getResourceAsStream(XMLConfigurationBackwardsCompatibilityTest.LATEST_FILENAME)));
         } catch (Exception e) {
             // Do nothing
